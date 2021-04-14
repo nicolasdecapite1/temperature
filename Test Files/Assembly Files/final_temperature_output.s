b0f3ac037e34c689fcc41a6214505766f185bd71
Mia de Leon <mia.de.leon@duke.edu>
2021-04-14 16:58:12 -0400

    added logic to mips file for control signal to heater circuit

--- Test Files/Assembly Files/final_temperature_output.s
+++ Test Files/Assembly Files/final_temperature_output.s
@@ -8,6 +8,10 @@ nop
 # B*(1/T-1/T0) = ln(R/R0)
 # e ^ (B*(1/T-1/T0)) * R0 = R , r29 holds T
 
+addi $r20, $r0, 10 #r20 will hold the low temperature boundary (if less than 10, turn heater on)
+addi $r21, $r0, 20 #r21 will hold the high temperature boundary (if greater than 20, turn heater off)
+	
+loop:	
 addi $r1, $r1, 3539 # B
 addi $r2, $r2, 2000 # R0
 addi $r3, $r3, 25   # T0
@@ -29,4 +33,16 @@ addi $r12, $r12, 10000 # 10000
 add $r13, $r9, $r12
 div $r14, $r9, $r13 
 mul $r15, $r14, $r11 # r15 holds V
+
+blt $r15, r20, turnHeaterOn
+bgt $r15, r21, turn HeaterOff
+
+jump loop
  
+turnHeaterOn:
+#custom instruction to turn heater on
+jump loop
+
+turnHeaterOff:
+#custom instruction to turn heater off
+jump loop
