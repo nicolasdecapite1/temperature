67cb4b4ed7b5107924c9786d2f52e9da45f74a5a
nicolasdecapite1 <nicolas.decapite@gmail.com>
2021-04-19 15:35:33 -0400

    having problems with the temp and decimal values

--- voltage.v
+++ voltage.v
@@ -1,21 +1,24 @@
 module voltage(v_therm, temp_therm);
-    input [31:0] v_therm;
+    input [3:0] v_therm;
     output [31:0] temp_therm;
 
-    wire w1, w2, w3, w4, w5, w6, R, B, T0, R0;
+    real v1;
+    assign v1 = v_therm;
+
+    wire [31:0] w1, w2, w3, w4, w5, w6, R, B, T0, R0;
     // 0000000000000000 
     assign T0 = 32'b000000000000000000000000011001; // 25 = 011001 , R0
     assign R0 = 32'b000000000000000000011111010000;   // 2000 
     assign B2 = 32'b000000000000000000110111010011;   // 3539 , B
     assign w1 = 32'b000000000000000010011100010000; //10000
-    assign w2 = 32'b000000000000000000000000011_01;   // 3.25 .. technically 3.3 tho
+    assign w2 = 32'b000000000000000000000000000011;   // 3.25 .. technically 3.3 tho
 
     assign w3 = w1 / w2;
-    assign w4 = v_therm / w2;
-    assign w5 = 1 - w4;
+    assign w4 = v1 / w2;
+    assign w5 = 1 + w4;
     assign w6 = w3 * w5;
 
-    assign R = w6*v_therm; // R
+    assign R = w6*v1; // R
 
     wire q1, q2, q3, q4, q5, q6;
 
--- voltage_tb.v
+++ voltage_tb.v
@@ -1,24 +1,31 @@
-// `timescale 1 ns / 100 ps
-// module voltage_tb;
+`timescale 1 ns / 100 ps
+module voltage_tb;
 
-//     reg input_voltage; // input
-//     wire output_temp; // output
+    wire [3:0] input_voltage; // input
+    wire output_temp; // output
 
-//     voltage voltage(.v_therm(input_voltage), .temp_therm(output_temp));
+    voltage voltage(.v_therm(input_voltage), .temp_therm(output_temp));
 
-//     // initialize
-//     begin
-//         input_voltage = 0;
+    // initialize
+    initial begin
+        input_voltage = 0;
 
-//         #80
-//         $finish
-//     end
+        // Output file name
+		$dumpfile("voltage.vcd");
+		// Module to capture and what level, 0 means all wires
+		$dumpvars(0, voltage_tb);
 
-//     always 
-//         #20 input_voltage += 1;
+        #80
+        $finish;
+    end
 
-//     always @(input_voltage) begin
-//         #1;
-//         $display("input_voltage:%d ---> output_temp:%d", input_voltage, output_temp);
-//     end
-// endmodule
+    always 
+        #10 input_voltage += 1;
+
+    always @(input_voltage) begin
+        #1;
+        $display("input_voltage:%d ---> output_temp:%d", input_voltage, output_temp);
+    end
+
+    
+endmodule
