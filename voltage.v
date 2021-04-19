f952b66453879c1823226cf87f5cd86b06b2e81d
nicolasdecapite1 <nicolas.decapite@gmail.com>
2021-04-19 15:36:10 -0400

    having problems with the temp and decimal values

--- voltage.v
+++ voltage.v
@@ -2,8 +2,8 @@ module voltage(v_therm, temp_therm);
     input [3:0] v_therm;
     output [31:0] temp_therm;
 
-    real v1;
-    assign v1 = v_therm;
+    // real v1;
+    // assign v1 = v_therm;
 
     wire [31:0] w1, w2, w3, w4, w5, w6, R, B, T0, R0;
     // 0000000000000000 
@@ -14,11 +14,11 @@ module voltage(v_therm, temp_therm);
     assign w2 = 32'b000000000000000000000000000011;   // 3.25 .. technically 3.3 tho
 
     assign w3 = w1 / w2;
-    assign w4 = v1 / w2;
+    assign w4 = v_therm / w2;
     assign w5 = 1 + w4;
     assign w6 = w3 * w5;
 
-    assign R = w6*v1; // R
+    assign R = w6*v_therm; // R
 
     wire q1, q2, q3, q4, q5, q6;
 
