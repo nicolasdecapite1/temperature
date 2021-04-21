d214edb1841e15454fa774efa25d7565ba41bec1
nicolasdecapite1 <nicolas.decapite@gmail.com>
2021-04-21 18:28:09 -0400

    final_v1 is working bit stream

--- vga_controller.v
+++ vga_controller.v
@@ -5,46 +5,40 @@ module vga_controller(clock, reset, in0, in1, in2, in3, in4, in5, in6, in7, clki
     wire [31:0] data_temp;
     Wrapper wrapper(.clock(clock), .reset(reset), .in0(in0), .in1(in1), .in2(in2), .in3(in3), .in4(in4), .in5(in5), .in6(in6), .in7(in7), .clkin(clkin), .outSignal(outSignal), .clkout(clkout), .temp_out(data_temp));
 
-
-
-    // logic to determine if A, B, C, D, E, F, G should be on or off
-
-    reg [6:0] cathodeA, cathodeB;
-    
-    always @(posedge clock)
+    reg [7:0] cathodeA, cathodeB;
+   
+    always @(data_temp)
     begin
         case(data_temp[4:0])
         5'b00000: begin
-            cathodeA = 7'b0000001;
-            cathodeB = 7'b0000001;
+            //cathodeA <= 8'b00000011;
+            cathodeA <= 8'b00000011;
+            cathodeB <= 8'b00000010;
             end
         5'b10111: begin
-            cathodeA = 7'b0010010;
-            cathodeB = 7'b0000110;
+            //cathodeA <= 8'b00100100;
+            cathodeA <= 8'b10011111;
+            cathodeB <= 8'b00001100;
             end
         5'b11000: begin
-            cathodeA = 7'b0010010;
-            cathodeB = 7'b0100100;
+            //cathodeA <= 8'b00100100;
+            cathodeA <= 8'b01100101;
+            cathodeB <= 8'b01001000;
             end
         5'b11001: begin
-            cathodeA = 7'b0010010;
-            cathodeB = 7'b0100000;
+            //cathodeA <= 8'b00100100;
+            cathodeA <= 8'b00001101;
+            cathodeB <= 8'b01000000;
             end
         endcase
     end
 
-
- 
     output [7:0] out_anode;
-    output [6:0] out_cathode;
+    output [7:0] out_cathode;
 
-    assign out_cathode = clock ? cathodeA : cathodeB; 
-    assign out_anode = clock ? 7'b01111111 : 7'b10111111;
-
-    
-    
-    
-        
+    //assign out_cathode = clock ? cathodeA : cathodeB; 
+    assign out_cathode = cathodeA;
+    assign out_anode = clock ? 8'b01111111 : 8'b10111111;
 
 
 endmodule
